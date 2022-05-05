76217645b64fdb2d6af81577beba3dd0b88a15a8
Ponneri Nitheesh <ponnerinitheesh1234@gamil.com>
2022-05-05 13:24:14 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,43 @@
+insert into Credential_details values('admin123',0,'admin','A');
+insert into Credential_details values('ni1234',0,'nitheesh123','C');
+insert into Credential_details values('ni1235',1,'nitheesh2345','C');
+
+insert into profile_details values('Tirupathi','2000-02-20','Ni@.com','Nitheesh','male','female','Muthialpet',
+'9876543212','nitheesh123','605003','Tirupathi','NewStreet','ni1234');
+
+
+insert into flight_details values('12345','AirLines',101,85);
+insert into flight_details values('1234','AirLines1',120,25);
+insert into flight_details values('123','AirLines2',130,95);
+insert into flight_details values('12','AirLines3',150,75);
+
+
+
+insert into Route_details values('12344','Banglore',25,10000,'Chennai');
+insert into Route_details values('12345','Hyderbad',25,10000,'Chennai');
+insert into Route_details values('12346','Kerala',25,10000,'Chennai');
+insert into Route_details values('12347','Delhi',25,10000,'Chennai');
+insert into Route_details values('12348','Mumbai',25,10000,'Chennai');
+
+insert into Schedule_details values('1','5','9.20',3,'12345','12344');
+insert into Schedule_details values('2','5','9.20',3,'1234','12345');
+insert into Schedule_details values('3','6','11.20',4,'123','12348');
+insert into Schedule_details values('4','6','11.20',4,'123','12348');
+
+
+
+
+insert into creditcard_details values('1234567812345678','500000','22/11/2019','22/11/2025','ni1234');
+insert into creditcard_details values('1234567812345679','600000','22/11/2019','22/11/2025','ni1235');
+
+
+insert into reservation_details values('1',1,'2022-04-04','2022-04-06',3,500000,'ni1234','1');
+insert into reservation_details values('2',1,'2022-04-04','2022-04-06',3,500000,'ni1235','2');
+
+
+insert into passenger_details values(1,'21','male','nitheesh','45','1');
+insert into passenger_details values(2,'22','male','ravi','46','2');
+insert into passenger_details values(3,'23','male','ashok','46','2');
+
+
+
