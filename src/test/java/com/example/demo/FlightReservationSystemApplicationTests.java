f568fb6890272e9a34c954948aeb725c62f9c4ec
Ponneri Nitheesh <ponnerinitheesh1234@gamil.com>
2022-05-05 15:18:11 +0530

    commit done

--- src/test/java/com/example/demo/FlightReservationSystemApplicationTests.java
+++ src/test/java/com/example/demo/FlightReservationSystemApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@SpringBootConfiguration
 @SpringBootTest
+@ContextConfiguration
+
 class FlightReservationSystemApplicationTests {
 
 	@Test
